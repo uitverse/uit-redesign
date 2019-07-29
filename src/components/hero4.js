5d2f20b5fc1e708ea6fe7d43c906b845e38d468f
mint <53132491+test1-test1@users.noreply.github.com>
2019-07-30 01:05:35 +0630

    Update hero4.js

--- src/components/hero4.js
+++ src/components/hero4.js
@@ -8,8 +8,8 @@ const hero3 = () => (
     <div className="hero4--wrapper">
       <Fade up cascade duration={300}>
         <div className="quote">
-          “   the graduate students from our university are able to fulfill a
-          shortage of experts and professionals not only in Myanmar IT industry
+          “  the graduate students from our university are able to fulfill
+           a shortage of experts and professionals not only in Myanmar IT industry
           but also in the International   ”
         </div>
         <div className="quoter">Dr. Saw Sanda Aye, Rector</div>
