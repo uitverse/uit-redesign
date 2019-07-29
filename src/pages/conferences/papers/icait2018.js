963391e9e9a8340f04edef4a35c785bc28f93688
FrencoJobs <frenco.willian@gmail.com>
2019-07-29 23:37:52 +0630

    fixed: darkmode text unconsistency

--- src/pages/conferences/papers/icait2018.js
+++ src/pages/conferences/papers/icait2018.js
@@ -1,38 +1,40 @@
 import React from "react"
-
-import Layout from "../../../components//layout"
+import Fade from "react-reveal"
+import Layout from "../../../components/layout"
 import SEO from "../../../components/seo"
 
 import { data } from "../../../../data/papers"
 
 import "../../../styles/papers.scss"
 
-const icait2018Papers = () => (
+const icait2017Papers = () => (
   <Layout addMargin>
     <SEO title="ICAIT 2018 Papers" />
-    <h3>Papers Published in ICAIT 2018</h3>
+    <h3>Papers Published in ICAIT 2017</h3>
     {data.icait2018.map((inset, index) => (
       <div className="topic" key={index}>
         <h3>{inset.topic}</h3>
-        <div className="papers">
-          {inset.papers.map((item, i) => (
-            <div className="paper" key={i}>
-              <div>
-                <div className="flexbox">
-                  <p>{item.title}</p>
-                  <a href={item.link}>Download</a>
+        <Fade up cascade>
+          <div className="papers">
+            {inset.papers.map((item, i) => (
+              <div className="paper" key={i}>
+                <div>
+                  <div className="flexbox">
+                    <p>{item.title}</p>
+                    <a href={item.link}>Download</a>
+                  </div>
+                  <p>
+                    <strong>{item.authors.join(", ")}</strong>
+                  </p>
                 </div>
-                <p>
-                  <strong>{item.authors.join(", ")}</strong>
-                </p>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </Fade>
         <hr />
       </div>
     ))}
   </Layout>
 )
 
-export default icait2018Papers
+export default icait2017Papers
