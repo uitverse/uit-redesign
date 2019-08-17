55f72c95ba7e887a9ad75bbc9ffb8f0cd5b13796
FrencoJobs <frenco.willian@gmail.com>
2019-08-17 22:38:44 +0630

    enhancement: hero2 transition

--- src/components/hero2.js
+++ src/components/hero2.js
@@ -6,9 +6,9 @@ import "../styles/latestnews.scss"
 const LatestNews = () => (
   <section className="latestnews">
     <div className="wrapper">
-      <Fade up>
+      <Fade up duration={300} distance="20%">
         <h3 className="header">Latest News</h3>
-        <Fade up cascade duration={300} distance="30%">
+        <Fade up duration={300} distance="20%">
           <div className="news">
             {data.map((item, index) => {
               return (
